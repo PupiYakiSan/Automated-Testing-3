6a30521418ea609ab200b17157490c00cc22f051
Olga <o.n.p@mail.ru>
2023-07-21 16:36:54 +0300

    Add test

---
+++ src/test/java/ru/netology/AlfaCardTest.java
@@ -0,0 +1,53 @@
+package ru.netology;
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+
+public class AlfaCardTest {
+
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setUpAll() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void SuccessSubmitForm() {
+        open("http://localhost:9999");
+
+        $("[data-test-id=name] input").val("Петров Иван");
+        $("[data-test-id=phone] input").val("+79001112233");
+        $("[data-test-id=agreement]").click();
+        $(By.className("button")).click();
+
+        String actual = $("[data-test-id=order-success]").getText().trim();
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        Assertions.assertEquals(expected, actual);
+    }
+
+}
