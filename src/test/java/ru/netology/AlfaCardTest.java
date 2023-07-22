fef126f5e4ebb622c4efb76b80fff2cf01e62aab
Olga <o.n.p@mail.ru>
2023-07-22 18:30:04 +0300

    Add tests

--- src/test/java/ru/netology/AlfaCardTest.java
+++ src/test/java/ru/netology/AlfaCardTest.java
@@ -1,15 +1,16 @@
 package ru.netology;
 
 
+import com.codeborne.selenide.ElementsCollection;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.Color;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 
 public class AlfaCardTest {
@@ -37,7 +38,7 @@ public class AlfaCardTest {
     }
 
     @Test
-    void SuccessSubmitForm() {
+    void successSubmitForm() {
         open("http://localhost:9999");
 
         $("[data-test-id=name] input").val("Петров Иван");
@@ -47,6 +48,175 @@ public class AlfaCardTest {
 
         String actual = $("[data-test-id=order-success]").getText().trim();
         String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void successSubmitFormDash() {
+        open("http://localhost:9999");
+
+        $("[data-test-id=name] input").val("Краснова Анна-Мария");
+        $("[data-test-id=phone] input").val("+79001112233");
+        $("[data-test-id=agreement]").click();
+        $(By.className("button")).click();
+
+        String actual = $("[data-test-id=order-success]").getText().trim();
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void nameEnglish() {
+        open("http://localhost:9999");
+
+        $("[data-test-id=name] input").val("Petrov Ivan");
+        $("[data-test-id=phone] input").val("+79001112233");
+        $("[data-test-id=agreement]").click();
+        $(By.className("button")).click();
+
+        ElementsCollection elements = $$(By.className("input__sub"));
+        String actual = elements.get(0).getText().trim();
+        String expected = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void nameNumber() {
+        open("http://localhost:9999");
+
+        $("[data-test-id=name] input").val("Иванов2 Тимофей2");
+        $("[data-test-id=phone] input").val("+79001112233");
+        $("[data-test-id=agreement]").click();
+        $(By.className("button")).click();
+
+        ElementsCollection elements = $$(By.className("input__sub"));
+        String actual = elements.get(0).getText().trim();
+        String expected = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void nameSymbol() {
+        open("http://localhost:9999");
+
+        $("[data-test-id=name] input").val("Николаев Андрей%");
+        $("[data-test-id=phone] input").val("+79001112233");
+        $("[data-test-id=agreement]").click();
+        $(By.className("button")).click();
+
+        ElementsCollection elements = $$(By.className("input__sub"));
+        String actual = elements.get(0).getText().trim();
+        String expected = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void phoneStartEight () {
+        open("http://localhost:9999");
+
+        $("[data-test-id=name] input").val("Ирина Маслова");
+        $("[data-test-id=phone] input").val("89001112233");
+        $("[data-test-id=agreement]").click();
+        $(By.className("button")).click();
+
+        ElementsCollection elements = $$(By.className("input__sub"));
+        String actual = elements.get(1).getText().trim();
+        String expected = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void phoneSmallNumber () {
+        open("http://localhost:9999");
+
+        $("[data-test-id=name] input").val("Ирина Маслова");
+        $("[data-test-id=phone] input").val("+7900111223");
+        $("[data-test-id=agreement]").click();
+        $(By.className("button")).click();
+
+        ElementsCollection elements = $$(By.className("input__sub"));
+        String actual = elements.get(1).getText().trim();
+        String expected = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void phoneBigNumber () {
+        open("http://localhost:9999");
+
+        $("[data-test-id=name] input").val("Ирина Маслова");
+        $("[data-test-id=phone] input").val("+790011122334");
+        $("[data-test-id=agreement]").click();
+        $(By.className("button")).click();
+
+        ElementsCollection elements = $$(By.className("input__sub"));
+        String actual = elements.get(1).getText().trim();
+        String expected = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void phoneWhitespace () {
+        open("http://localhost:9999");
+
+        $("[data-test-id=name] input").val("Ирина Маслова");
+        $("[data-test-id=phone] input").val("+7 9001112233");
+        $("[data-test-id=agreement]").click();
+        $(By.className("button")).click();
+
+        ElementsCollection elements = $$(By.className("input__sub"));
+        String actual = elements.get(1).getText().trim();
+        String expected = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void phoneSymbol () {
+        open("http://localhost:9999");
+
+        $("[data-test-id=name] input").val("Ирина Маслова");
+        $("[data-test-id=phone] input").val("+7900111223@");
+        $("[data-test-id=agreement]").click();
+        $(By.className("button")).click();
+
+        ElementsCollection elements = $$(By.className("input__sub"));
+        String actual = elements.get(1).getText().trim();
+        String expected = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void phoneLetter () {
+        open("http://localhost:9999");
+
+        $("[data-test-id=name] input").val("Ирина Маслова");
+        $("[data-test-id=phone] input").val("+7A00111223");
+        $("[data-test-id=agreement]").click();
+        $(By.className("button")).click();
+
+        ElementsCollection elements = $$(By.className("input__sub"));
+        String actual = elements.get(1).getText().trim();
+        String expected = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void notAgreement() {
+        open("http://localhost:9999");
+
+        $("[data-test-id=name] input").val("Петров Иван");
+        $("[data-test-id=phone] input").val("+79001112233");
+        $(By.className("button")).click();
+
+        String actual = $(By.className("input_invalid")).getCssValue("color");
+        actual = Color.fromString(actual).asHex();
+        String expected = "#ff5c5c";
         Assertions.assertEquals(expected, actual);
     }
 
