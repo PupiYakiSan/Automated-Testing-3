4cd518eba6d2c255ffb5df63e727d4ba0024fd6a
Olga <o.n.p@mail.ru>
2023-07-24 19:10:53 +0300

    Update tests

--- src/test/java/ru/netology/AlfaCardTest.java
+++ src/test/java/ru/netology/AlfaCardTest.java
@@ -1,223 +1,170 @@
 package ru.netology;
 
 
-import com.codeborne.selenide.ElementsCollection;
-import io.github.bonigarcia.wdm.WebDriverManager;
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.Color;
 
 import static com.codeborne.selenide.Selenide.*;
 
 
 public class AlfaCardTest {
 
-    private WebDriver driver;
-
-    @BeforeAll
-    static void setUpAll() {
-        WebDriverManager.chromedriver().setup();
-    }
-
     @BeforeEach
-    void setUp() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--headless");
-        driver = new ChromeDriver(options);
-    }
-
-    @AfterEach
-    void tearDown() {
-        driver.quit();
-        driver = null;
+    public void openLocalhost() {
+        open("http://localhost:9999");
     }
 
     @Test
     void successSubmitForm() {
-        open("http://localhost:9999");
 
         $("[data-test-id=name] input").val("Петров Иван");
         $("[data-test-id=phone] input").val("+79001112233");
         $("[data-test-id=agreement]").click();
         $(By.className("button")).click();
 
-        String actual = $("[data-test-id=order-success]").getText().trim();
-        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
-        Assertions.assertEquals(expected, actual);
+        $("[data-test-id=order-success]").shouldHave(Condition.exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 
     @Test
     void successSubmitFormDash() {
-        open("http://localhost:9999");
 
         $("[data-test-id=name] input").val("Краснова Анна-Мария");
         $("[data-test-id=phone] input").val("+79001112233");
         $("[data-test-id=agreement]").click();
         $(By.className("button")).click();
 
-        String actual = $("[data-test-id=order-success]").getText().trim();
-        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
-        Assertions.assertEquals(expected, actual);
+        $("[data-test-id=order-success]").shouldHave(Condition.exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 
     @Test
     void nameEnglish() {
-        open("http://localhost:9999");
 
         $("[data-test-id=name] input").val("Petrov Ivan");
         $("[data-test-id=phone] input").val("+79001112233");
         $("[data-test-id=agreement]").click();
         $(By.className("button")).click();
 
-        ElementsCollection elements = $$(By.className("input__sub"));
-        String actual = elements.get(0).getText().trim();
-        String expected = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
-        Assertions.assertEquals(expected, actual);
+        $("[data-test-id='name'].input_invalid .input__sub").shouldHave(Condition.exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
 
     @Test
     void nameNumber() {
-        open("http://localhost:9999");
 
         $("[data-test-id=name] input").val("Иванов2 Тимофей2");
         $("[data-test-id=phone] input").val("+79001112233");
         $("[data-test-id=agreement]").click();
         $(By.className("button")).click();
 
-        ElementsCollection elements = $$(By.className("input__sub"));
-        String actual = elements.get(0).getText().trim();
-        String expected = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
-        Assertions.assertEquals(expected, actual);
+        $("[data-test-id='name'].input_invalid .input__sub").shouldHave(Condition.exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
 
     @Test
     void nameSymbol() {
-        open("http://localhost:9999");
 
         $("[data-test-id=name] input").val("Николаев Андрей%");
         $("[data-test-id=phone] input").val("+79001112233");
         $("[data-test-id=agreement]").click();
         $(By.className("button")).click();
 
-        ElementsCollection elements = $$(By.className("input__sub"));
-        String actual = elements.get(0).getText().trim();
-        String expected = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
-        Assertions.assertEquals(expected, actual);
+        $("[data-test-id='name'].input_invalid .input__sub").shouldHave(Condition.exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    void nameEmpty() {
+
+        $("[data-test-id=phone] input").val("+79001112233");
+        $("[data-test-id=agreement]").click();
+        $(By.className("button")).click();
+
+        $("[data-test-id='name'].input_invalid .input__sub").shouldHave(Condition.exactText("Поле обязательно для заполнения"));
     }
 
     @Test
     void phoneStartEight () {
-        open("http://localhost:9999");
 
         $("[data-test-id=name] input").val("Ирина Маслова");
         $("[data-test-id=phone] input").val("89001112233");
         $("[data-test-id=agreement]").click();
         $(By.className("button")).click();
 
-        ElementsCollection elements = $$(By.className("input__sub"));
-        String actual = elements.get(1).getText().trim();
-        String expected = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
-
-        Assertions.assertEquals(expected, actual);
+        $("[data-test-id='phone'].input_invalid .input__sub").shouldHave(Condition.exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
 
     @Test
     void phoneSmallNumber () {
-        open("http://localhost:9999");
 
         $("[data-test-id=name] input").val("Ирина Маслова");
         $("[data-test-id=phone] input").val("+7900111223");
         $("[data-test-id=agreement]").click();
         $(By.className("button")).click();
 
-        ElementsCollection elements = $$(By.className("input__sub"));
-        String actual = elements.get(1).getText().trim();
-        String expected = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
-
-        Assertions.assertEquals(expected, actual);
+        $("[data-test-id='phone'].input_invalid .input__sub").shouldHave(Condition.exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
 
     @Test
     void phoneBigNumber () {
-        open("http://localhost:9999");
 
         $("[data-test-id=name] input").val("Ирина Маслова");
         $("[data-test-id=phone] input").val("+790011122334");
         $("[data-test-id=agreement]").click();
         $(By.className("button")).click();
 
-        ElementsCollection elements = $$(By.className("input__sub"));
-        String actual = elements.get(1).getText().trim();
-        String expected = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
-
-        Assertions.assertEquals(expected, actual);
+        $("[data-test-id='phone'].input_invalid .input__sub").shouldHave(Condition.exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
 
     @Test
     void phoneWhitespace () {
-        open("http://localhost:9999");
 
         $("[data-test-id=name] input").val("Ирина Маслова");
         $("[data-test-id=phone] input").val("+7 9001112233");
         $("[data-test-id=agreement]").click();
         $(By.className("button")).click();
 
-        ElementsCollection elements = $$(By.className("input__sub"));
-        String actual = elements.get(1).getText().trim();
-        String expected = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
-
-        Assertions.assertEquals(expected, actual);
+        $("[data-test-id='phone'].input_invalid .input__sub").shouldHave(Condition.exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
 
     @Test
     void phoneSymbol () {
-        open("http://localhost:9999");
 
         $("[data-test-id=name] input").val("Ирина Маслова");
         $("[data-test-id=phone] input").val("+7900111223@");
         $("[data-test-id=agreement]").click();
         $(By.className("button")).click();
 
-        ElementsCollection elements = $$(By.className("input__sub"));
-        String actual = elements.get(1).getText().trim();
-        String expected = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
-
-        Assertions.assertEquals(expected, actual);
+        $("[data-test-id='phone'].input_invalid .input__sub").shouldHave(Condition.exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
 
     @Test
     void phoneLetter () {
-        open("http://localhost:9999");
 
         $("[data-test-id=name] input").val("Ирина Маслова");
         $("[data-test-id=phone] input").val("+7A00111223");
         $("[data-test-id=agreement]").click();
         $(By.className("button")).click();
 
-        ElementsCollection elements = $$(By.className("input__sub"));
-        String actual = elements.get(1).getText().trim();
-        String expected = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
+        $("[data-test-id='phone'].input_invalid .input__sub").shouldHave(Condition.exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
 
-        Assertions.assertEquals(expected, actual);
+    @Test
+    void phoneEmpty () {
+
+        $("[data-test-id=name] input").val("Ирина Маслова");
+        $("[data-test-id=agreement]").click();
+        $(By.className("button")).click();
+
+        $("[data-test-id='phone'].input_invalid .input__sub").shouldHave(Condition.exactText("Поле обязательно для заполнения"));
     }
 
     @Test
     void notAgreement() {
-        open("http://localhost:9999");
 
         $("[data-test-id=name] input").val("Петров Иван");
         $("[data-test-id=phone] input").val("+79001112233");
         $(By.className("button")).click();
 
-        String actual = $(By.className("input_invalid")).getCssValue("color");
-        actual = Color.fromString(actual).asHex();
-        String expected = "#ff5c5c";
-        Assertions.assertEquals(expected, actual);
+        $("[data-test-id='agreement'].input_invalid .checkbox__text").shouldBe(Condition.visible);
+
     }
 
 }
